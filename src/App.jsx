2dd261a08801e78ff9f971393682c3e5e489a221
YuriAres <49327925+YuriAres@users.noreply.github.com>
2021-10-17 18:22:34 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Components/Home/Header'
+import Body from './Components/Home/Body'
 
 function App() {
   return (
     <>
         <Header/>
+        <Body/>
     </>
   );
 }
