4e61998423d2609894bf9a0c5d2b7e56334cc284
YuriAres <49327925+YuriAres@users.noreply.github.com>
2021-10-17 17:40:17 -0300

    Update Header.jsx

--- src/Components/Home/Header.jsx
+++ src/Components/Home/Header.jsx
@@ -1,17 +1,22 @@
 import './css/home.css'
+import Logo from '../../Images/Home/logo.png'
 export default props => {
     return (
         <>
             <header> 
                 <div className="AreaMenu">
                     <div className="Left">
-                        A
+                        <img src={Logo} className="ImageLogo"/>
+                        <span className="Title">Soccer<strong>STATS</strong></span>
                     </div>
                     <div className="Right">
-                        A
+                        <span className="MenuOptions"><a href="">HOME</a></span>
+                        <span className="MenuOptions"><a href="">ESTATISTICAS</a></span>
+                        <span className="MenuOptions"><a href="">QUEM SOMOS</a></span>
                     </div>
                 </div>
             </header>
+            <div className="MainArea"></div>
         </>
     );
 }
