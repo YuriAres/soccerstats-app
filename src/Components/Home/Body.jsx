77731c2190ec5d57fb2450165295f4a8dd1bfb57
YuriAres <49327925+YuriAres@users.noreply.github.com>
2021-10-17 18:22:37 -0300

    Create Body.jsx

---
+++ src/Components/Home/Body.jsx
@@ -0,0 +1,11 @@
+import './css/home.css'
+
+export default props => {
+    return (
+        <>
+            <div className="MainArea">
+                <div className="Title1">CAMPEONATOS 2021</div>
+                </div>
+        </>
+    );
+}
