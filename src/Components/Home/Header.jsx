4a9f821304d467b6afb6f499c44d0529d5cb9adf
YuriAres <49327925+YuriAres@users.noreply.github.com>
2021-10-18 03:35:28 -0300

    Update Header.jsx

--- src/Components/Home/Header.jsx
+++ src/Components/Home/Header.jsx
@@ -1,5 +1,7 @@
 import './css/home.css'
 import Logo from '../../Images/Home/logo.png'
+import { Link } from 'react-router-dom';
+
 export default props => {
     return (
         <>
@@ -10,9 +12,9 @@ export default props => {
                         <span className="Title">Soccer<strong>STATS</strong></span>
                     </div>
                     <div className="Right">
-                        <span className="MenuOptions"><a href="">HOME</a></span>
-                        <span className="MenuOptions"><a href="">ESTATISTICAS</a></span>
-                        <span className="MenuOptions"><a href="">QUEM SOMOS</a></span>
+                        <span className="MenuOptions"><Link to="/">HOME</Link></span>
+                        <span className="MenuOptions"><Link to="/Classificacao">ESTATISTICAS</Link></span>
+                        <span className="MenuOptions"><Link to="/QuemSomos">QUEM SOMOS</Link></span>
                     </div>
                 </div>
             </header>
