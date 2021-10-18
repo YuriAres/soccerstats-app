c0db7a033e1bdee19a114b130d4d622508e31807
YuriAres <49327925+YuriAres@users.noreply.github.com>
2021-10-18 13:46:07 -0300

    Update Body_About.jsx

--- src/Components/Home/Body_About.jsx
+++ src/Components/Home/Body_About.jsx
@@ -8,14 +8,15 @@ export default props => {
     <div className="MainArea">
         
         <div className="Title1">SOBRE NÓS</div>
-
-        <div>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc volutpat dictum elit, a viverra mi dapibus a. Suspendisse in maximus nibh. Ut viverra velit leo, ac semper sem sollicitudin quis. Proin sit amet semper enim, ac scelerisque ante. Praesent malesuada mollis metus at finibus. Suspendisse vehicula consectetur risus a fermentum. Duis vitae leo vestibulum, sagittis ex vel, auctor ante. 
-            Phasellus sed turpis ultricies, egestas sapien ut, porttitor tortor. Curabitur malesuada mauris ut elit volutpat maximus. 
-            Integer rutrum quam id nibh laoreet, scelerisque mollis lacus vehicula. Sed congue quis dolor sit amet lacinia. Ut dictum enim a tincidunt vulputate. Suspendisse consectetur ante sit amet odio bibendum porttitor. Aliquam porttitor pellentesque faucibus. Proin porta fermentum hendrerit.
-            </p>
-        </div>
+        <div className="Card">
+                    <div className="Title">
+                        DESCRIÇÃO DO SITE
+                    </div>
+                    <div className="ContentAbout">
+                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc volutpat dictum elit, a viverra mi dapibus a. Suspendisse in maximus nibh. Ut viverra velit leo, ac semper sem sollicitudin quis. Proin sit amet semper enim, ac scelerisque ante. Praesent malesuada mollis metus at finibus. Suspendisse vehicula consectetur risus a fermentum. Duis vitae leo vestibulum, sagittis ex vel, auctor ante. Phasellus sed turpis ultricies, egestas sapien ut, porttitor tortor. Curabitur malesuada mauris ut elit volutpat maximus. Integer rutrum quam id nibh laoreet, scelerisque mollis lacus vehicula. Sed congue quis dolor sit amet lacinia. Ut dictum enim a tincidunt vulputate. Suspendisse consectetur ante sit amet odio bibendum porttitor. 
+                           Aliquam porttitor pellentesque faucibus. Proin porta fermentum hendrerit.</p>
+                    </div>
+                </div>
     </div>
     </>
     );
