55ed159842600cef76be48d8ea5af853864f208c
YuriAres <49327925+YuriAres@users.noreply.github.com>
2021-10-17 23:58:53 -0300

    Update Body.jsx

--- src/Components/Home/Body.jsx
+++ src/Components/Home/Body.jsx
@@ -1,11 +1,47 @@
 import './css/home.css'
+import GetLeague from '../Axios (API)/GetLeague'
+
 
 export default props => {
+ 
     return (
         <>
+        
             <div className="MainArea">
                 <div className="Title1">CAMPEONATOS 2021</div>
+                <div className="Card">
+                        <div className="Title">
+                                AMÉRICA DO SUL
+                        </div>
+                        <div className="Content">
+                               <GetLeague></GetLeague>
+                        </div>
                 </div>
+                <div className="Card">
+                        <div className="Title">
+                                EUROPA
+                        </div>
+                        <div className="Content">
+                                A
+                        </div>
+                </div>
+                <div className="Card">
+                        <div className="Title">
+                                ÁSIA
+                        </div>
+                        <div className="Content">
+                                A
+                        </div>
+                </div>
+                <div className="Card">
+                        <div className="Title">
+                                ÁFRICA
+                        </div>
+                        <div className="Content">
+                                A
+                        </div>
+                </div>
+            </div>
         </>
     );
 }
