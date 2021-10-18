42f9413639d3ceb23bd39a02ec05922a5095e7a1
YuriAres <49327925+YuriAres@users.noreply.github.com>
2021-10-18 13:46:08 -0300

    Update Body_Ranking.jsx

--- src/Components/Home/Body_Ranking.jsx
+++ src/Components/Home/Body_Ranking.jsx
@@ -1,31 +1,36 @@
 import './css/home.css'
-import Ranking from '../Axios (API)/Rank' 
+import Ranking from '../Axios (API)/Rank'
+import { useParams } from 'react-router';
 
 export default props => {
+    const {ID,ABR} = useParams()
 
     return (<> 
         <div className="MainArea">
         
-        <div className="Title1">TABELA BRASILEIRÃO</div>
-        <table>
-          <thead>
-              <tr> 
-                <th>Pos.</th>
-                <th>Time</th>
-                <th>Pontos</th>
-                <th>Jogos</th>
-                <th>Vit.</th>
-                <th>Emp.</th>
-                <th>Der.</th>
-                <th>GP</th>
-                <th>GC</th>
-                <th>SG</th>
-              </tr> 
-          </thead>
-          <tbody>
-              <Ranking league="por.1"></Ranking>
-          </tbody>
-        </table>
+        <div className="Title1">{ID}</div>
+            <div className="Table">
+                <table>
+                    
+                    <thead>
+                        <tr> 
+                            <th>Pos.</th>
+                            <th>Time</th>
+                            <th>Pontos</th>
+                            <th>Jogos</th>
+                            <th>Vit.</th>
+                            <th>Emp.</th>
+                            <th>Der.</th>
+                            <th>GP</th>
+                            <th>GC</th>
+                            <th>SG</th>
+                        </tr> 
+                    </thead>
+                    <tbody>
+                        <Ranking league={ABR}></Ranking>
+                    </tbody>
+                </table>
+            </div>
         </div>
         
         </>);
