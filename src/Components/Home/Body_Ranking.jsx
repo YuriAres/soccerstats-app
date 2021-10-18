14ac3984d33cb6530e31d03eb4372c50e2cbf119
YuriAres <49327925+YuriAres@users.noreply.github.com>
2021-10-18 11:09:58 -0300

    Update Body_Ranking.jsx

--- src/Components/Home/Body_Ranking.jsx
+++ src/Components/Home/Body_Ranking.jsx
@@ -7,7 +7,25 @@ export default props => {
         <div className="MainArea">
         
         <div className="Title1">TABELA BRASILEIRÃO</div>
-            <Ranking league="eng.1"></Ranking>
+        <table>
+          <thead>
+              <tr> 
+                <th>Pos.</th>
+                <th>Time</th>
+                <th>Pontos</th>
+                <th>Jogos</th>
+                <th>Vit.</th>
+                <th>Emp.</th>
+                <th>Der.</th>
+                <th>GP</th>
+                <th>GC</th>
+                <th>SG</th>
+              </tr> 
+          </thead>
+          <tbody>
+              <Ranking league="por.1"></Ranking>
+          </tbody>
+        </table>
         </div>
         
         </>);
