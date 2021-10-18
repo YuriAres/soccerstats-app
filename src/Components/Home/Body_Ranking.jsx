41d88df2d283de93f090d74c782844dd25279804
YuriAres <49327925+YuriAres@users.noreply.github.com>
2021-10-18 03:35:26 -0300

    Create Body_Ranking.jsx

---
+++ src/Components/Home/Body_Ranking.jsx
@@ -0,0 +1,13 @@
+import './css/home.css'
+
+export default props => {
+
+
+    return (<> <div>Hello world!</div>
+    <div>Hello world!</div>
+    <div>Hello world!</div>
+    <div>Hello world!</div>
+    <div>Hello world!</div> </>);
+    
+
+}
