8722e650e8b5b683c94251b8528b2d5d1542ec6a
YuriAres <49327925+YuriAres@users.noreply.github.com>
2021-10-18 05:01:57 -0300

    Create GetRanking.jsx

---
+++ src/Components/Axios (API)/GetRanking.jsx
@@ -0,0 +1,40 @@
+import API from './Getinfo'
+import './css/API.css'
+import React, { useState, useEffect } from 'react';
+
+export default props => {
+  const [league, setLeague] = useState({});
+
+  useEffect(() => {
+    API
+      .get('/leagues/'+props.league+'/standings?season=2020&sort=asc')
+      .then((response) => setLeague(response.data))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
+
+  return (
+    <>
+      {(typeof league.data != 'undefined') ? (<> 
+            <tr>
+                <td>{league.data.standings[props.id].stats[8].displayValue}</td>
+                <td>{league.data.standings[props.id].team.name}</td>
+                <td>{league.data.standings[props.id].stats[6].displayValue}</td>
+                <td>{league.data.standings[props.id].stats[3].displayValue}</td>
+                <td>{league.data.standings[props.id].stats[0].displayValue}</td>
+                <td>{league.data.standings[props.id].stats[2].displayValue}</td>
+                <td>{league.data.standings[props.id].stats[1].displayValue}</td>
+                <td>{league.data.standings[props.id].stats[4].displayValue}</td>
+                <td>{league.data.standings[props.id].stats[5].displayValue}</td>
+                <td>{league.data.standings[props.id].stats[9].displayValue}</td>
+            </tr>
+        {console.log(league.data.standings[0])}
+
+
+        
+        </>) : ("")}
+    </>
+  )
+
+}
