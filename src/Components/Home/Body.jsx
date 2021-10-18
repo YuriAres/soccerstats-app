370e6fa4b81df0d305943dca38898dbe0aaac0c0
YuriAres <49327925+YuriAres@users.noreply.github.com>
2021-10-18 01:29:05 -0300

    Update Body.jsx

--- src/Components/Home/Body.jsx
+++ src/Components/Home/Body.jsx
@@ -3,7 +3,7 @@ import GetLeague from '../Axios (API)/GetLeague'
 
 
 export default props => {
- 
+
     return (
         <>
         
@@ -11,10 +11,12 @@ export default props => {
                 <div className="Title1">CAMPEONATOS 2021</div>
                 <div className="Card">
                         <div className="Title">
-                                AMÉRICA DO SUL
+                                AMÉRICAS
                         </div>
                         <div className="Content">
-                               <GetLeague></GetLeague>
+                                <GetLeague id="0"/>
+                                <GetLeague id="2"/>
+                                <GetLeague id="12"/>
                         </div>
                 </div>
                 <div className="Card">
@@ -22,7 +24,14 @@ export default props => {
                                 EUROPA
                         </div>
                         <div className="Content">
-                                A
+                                <GetLeague id="4"/>
+                                <GetLeague id="5"/>
+                                <GetLeague id="6"/>
+                                <GetLeague id="7"/>
+                                <GetLeague id="9"/>
+                                <GetLeague id="13"/>
+                                <GetLeague id="14"/>
+                                <GetLeague id="16"/>
                         </div>
                 </div>
                 <div className="Card">
@@ -30,15 +39,22 @@ export default props => {
                                 ÁSIA
                         </div>
                         <div className="Content">
-                                A
+                                <GetLeague id="3"/>
+                                <GetLeague id="8"/>
+                                <GetLeague id="10"/>
+                                <GetLeague id="11"/>
+                                <GetLeague id="15"/>
+                                <GetLeague id="17"/>
+                                <GetLeague id="18"/>
                         </div>
                 </div>
                 <div className="Card">
                         <div className="Title">
-                                ÁFRICA
+                                ÁFRICA E OCEÂNIA
                         </div>
                         <div className="Content">
-                                A
+                                <GetLeague id="1"/>
+                                <GetLeague id="19"/>
                         </div>
                 </div>
             </div>
