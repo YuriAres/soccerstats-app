3faff6088911500562d7e3d3b4cbb17e89ec7c88
YuriAres <49327925+YuriAres@users.noreply.github.com>
2021-10-18 03:35:18 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css';
 import Header from './Components/Home/Header'
 import Body from './Components/Home/Body'
+import {BrowserRouter as Router} from 'react-router-dom'
 
 function App() {
   return (
-    <>
+    <>  
+      <Router>
         <Header/>
         <Body/>
+      </Router>
     </>
   );
 }
