e30330f5fa8cb4fc5295606bf1ec0eb46a46ab24
YuriAres <49327925+YuriAres@users.noreply.github.com>
2021-10-18 01:29:03 -0300

    Update GetLeague.jsx

--- src/Components/Axios (API)/GetLeague.jsx
+++ src/Components/Axios (API)/GetLeague.jsx
@@ -1,24 +1,34 @@
 import API from './Getinfo'
-import React, {useState, useEffect} from 'react';
+import './css/API.css'
+import React, { useState, useEffect } from 'react';
 
 export default props => {
-    const [league, setLeague] = useState({});
+  const [league, setLeague] = useState({});
 
-    useEffect(() => {
-      API
-          .get('/leagues')
-          .then((response) => setLeague(response.data))
-          .catch((err) => {
-            console.error("ops! ocorreu um erro" + err);
-          });
-    },[]);
+  useEffect(() => {
+    API
+      .get('/leagues')
+      .then((response) => setLeague(response.data))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
 
-    return(
-        <>  
-            {(typeof league.data != 'undefined') ? (
-            console.log(league.data[0].logos.dark)) : ("")}
-            
-        </>
-    )
+  return (
+    <>
+      {(typeof league.data != 'undefined') ? (<> 
+      <div className="LeagueDiv">
+          <div className="LeagueImage">
+            <img src={league.data[props.id].logos.light} className="LeagueIcons" />
+          </div>
+          <div className="LeagueDescription">
+            {league.data[props.id].name}
+            <div>({league.data[props.id].abbr})</div>
+          </div>
+          <button className="LeagueButton">Classificação</button>
+      </div></>) : ("")}
+
+    </>
+  )
 
 }
